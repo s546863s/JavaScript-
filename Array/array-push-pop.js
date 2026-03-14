9b5cdcfec8ecb49d93afc21d8c5ff4b238e3a9ce
Abdus-Salam-814 <abdussalam6111997@gmail.com>
2026-03-14 11:17:49 +0600

    i learn push

--- Array/array-push-pop.js
+++ Array/array-push-pop.js
@@ -4,3 +4,23 @@ fruits.push("graoes");
 console.log(fruits); // Output: ["apple", "banana", "orange", "grapes"]
 
 // Removing the last element from the array
+
+const lastFruit = fruits.pop();
+console.log(lastFruit);
+
+
+
+console.log(fruits); // Output: ["apple", "banana", "orange"]
+
+// Adding multiple elements to the end of the array
+const moreFruits = ["kiwi", "mango"];
+fruits.push(...moreFruits);
+console.log(fruits); // Output: ["apple", "banana", "orange", "kiwi", "mango"]
+
+
+
+
+const moreFruits2 = ["pear", "peach", "plum", "pineapple", "watermelon", ];
+
+fruits.push(...moreFruits2);
+console.log(fruits); // Output: ["apple", "banana", "orange", "kiwi", "mango", "pear", "peach", "plum", "pineapple", "watermelon"]
